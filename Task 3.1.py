fc47ae7f5eeb3740a4fd8038220e7ac30f6c47d1
jonesbrett <bjones@cisilion.com>
2022-07-13 04:32:01 +0100

    Course projects

---
+++ Task 3.1.py
@@ -0,0 +1,11 @@
+import string
+
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate per Hours:")
+r = rate
+if h <= 40:
+    print(h * float(r))
+else:
+    print((((h - 40) * 1.5) + 40) * float(r))
