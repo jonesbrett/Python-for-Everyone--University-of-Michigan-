3c2b672aed241e4c57770f81c71e47c0a811df9d
jonesbrett <bjones@cisilion.com>
2022-07-14 15:38:51 +0100

    .

---
+++ Task 5.2.py
@@ -0,0 +1,28 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        inum = int(num)
+        # print(inum)
+        if largest is None or inum >= largest:
+            largest = inum
+            # print("largest is set to", largest)
+            continue
+
+        elif smallest is None or inum <= smallest:
+            smallest = inum
+            # print("smallest is set to", smallest)
+            continue
+
+        elif inum < largest or inum > smallest:
+            # print("not bigger or smaller")
+            continue
+    except:
+        print("Invalid input")
+        continue
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
