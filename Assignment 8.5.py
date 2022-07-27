99662e40cd4747779dd2da78184edd4835fb6f88
jonesbrett <bjones@cisilion.com>
2022-07-27 09:09:39 +0100

    .

--- Assignment 8.5.py
+++ Assignment 8.5.py
@@ -1,8 +1,17 @@
 fname = input("Enter file name: ")
-if len(fname) < 1:
-    fname = "mbox-short.txt"
-
 fh = open(fname)
 count = 0
+# if len(fname) < 1:
+# fname = "mbox-short.txt"
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith("From "):
+        continue
+
+    else:
+        word = line.split()
+        print(word[1])
+        count = count + 1
+
 
 print("There were", count, "lines in the file with From as the first word")
