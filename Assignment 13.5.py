45f2317b299401c4d683cc5c6861896b2320e62a
jonesbrett <bjones@cisilion.com>
2022-08-16 08:02:39 +0100

    .

--- Assignment 13.5.py
+++ Assignment 13.5.py
@@ -42,3 +42,44 @@
 
 # Make sure to retreive the data from the URL specified above and not the normal Google API. Your program
 # should work with the Google API - but the place_id may not match for this assignment.
+
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+
+serviceurl = "http://py4e-data.dr-chuck.net/json?"
+
+while True:
+    address = input("Enter location: ")
+    if len(address) < 1:
+        break
+
+    #   parms = dict()
+    #    parms["address"] = address
+    #    if api_key is not False:
+    #        parms["key"] = api_key
+    url = serviceurl + urllib.parse.urlencode(address)
+
+    print("Retrieving", url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print("Retrieved", len(data), "characters")
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or "status" not in js or js["status"] != "OK":
+        print("==== Failure To Retrieve ====")
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js["results"][0]["geometry"]["location"]["lat"]
+    lng = js["results"][0]["geometry"]["location"]["lng"]
+    print("lat", lat, "lng", lng)
+    location = js["results"][0]["formatted_address"]
+    print(location)
