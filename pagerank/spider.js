e118196cab3852904f208d64904687c4ffa7f79f
jonesbrett <bjones@cisilion.com>
2022-08-26 15:50:31 +0100

    Sorting into courses

--- pagerank/spider.js
+++
@@ -1,6 +0,0 @@
-spiderJson = {
-    "nodes": [
-        { "weight": 1, "rank": 0.266634799081, "id": 4267, "url": "https://lti-tools.dr-chuck.com/tsugi/mod/python-data/data/known_by_Declain.html" }],
-    "links": [
-        { "source": 98, "target": 97, "value": 3 }]
-};
